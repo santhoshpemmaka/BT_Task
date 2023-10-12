4f2caad8c39e314d39549e3b5df01b6568291d28
Santhosh_Pemmaka <30383739+santhoshpemmaka@users.noreply.github.com>
2023-10-12 18:29:55 +0530

    Update app.js

--- server_bt/app.js
+++ server_bt/app.js
@@ -27,6 +27,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "*");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Max-Age", "86400");
   req.next();
 });
 
