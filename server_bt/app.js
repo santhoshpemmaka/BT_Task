e2af3aeedebf4d53a9a2626e4a585b914d57f030
Santhosh_Pemmaka <30383739+santhoshpemmaka@users.noreply.github.com>
2023-10-18 11:24:17 +0530

    Update app.js

--- server_bt/app.js
+++ server_bt/app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
 
 const feedbackRoutes = require("./router/feedBack");
 const userRoutes = require("./router/user");
